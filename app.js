f49611d5e116f81073378fd7acdb5fc35792fbc7
nishant1596 <happysoni777@gmail.com>
2019-10-08 13:08:20 +0530

    Shuffle functionality added

--- app.js
+++ app.js
@@ -10,8 +10,20 @@ var imageArray=[
   "images/nine.jpg",
   "images/ten.jpg"
 ];
+function shuffle(arra1) {
+  var ctr = arra1.length, temp, index;
+  while (ctr > 0) {
+      index = Math.floor(Math.random() * ctr);
+      ctr--;
+      temp = arra1[ctr];
+      arra1[ctr] = arra1[index];
+      arra1[index] = temp;
+  }
+  return arra1;
+}
 
 $(document).ready(function() {
+shuffle(imageArray);
   for(let i=1;i<3;i++){
     $.each(imageArray,function(index, el) {
       $("#images").append(`<div id="card${i}${index}"><img src="${el}" ></div>`)
