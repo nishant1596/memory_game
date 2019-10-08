ddecb6375f0722e7ef509dd661be50650229d1ae
nishant1596 <happysoni777@gmail.com>
2019-10-08 23:32:16 +0530

    reset remaining

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ var imageArray=[
   "images/nine.jpg",
   "images/ten.jpg"
 ];
+function concatArray(imageArray){
+  newArray=$.merge($.merge([],imageArray),imageArray);
+  return newArray;
+}
 function shuffle(arra1) {
   var ctr = arra1.length, temp, index;
   while (ctr > 0) {
@@ -22,11 +26,65 @@ function shuffle(arra1) {
   return arra1;
 }
 
+
+
+  var moves=0;
+  var arrLen=0;
+  var srcCount=0;
+  var srcArray=[];
+  var winpoint=0;
+  var timer = 4;
+
+
+
+
+
 $(document).ready(function() {
-shuffle(imageArray);
-  for(let i=1;i<3;i++){
-    $.each(imageArray,function(index, el) {
-      $("#images").append(`<div id="card${i}${index}"><img src="${el}" ></div>`)
+  const newArray=concatArray(imageArray);
+  shuffle(newArray);
+    $.each(newArray,function(index, el) {
+      $("#images").append(`<div class="card"><img src="${el}" id="${index}"></div>`);
     });
-  }
+
+    setInterval(function () {
+      if (timer>0) {
+        $("#timer").text(timer);
+        --timer;
+      }
+      else{
+        $("#timer").text("");
+      }
+    },1000)
+    setTimeout(function () {
+      $("#images div img").hide();
+    },5000)
+
+    $(".card").click(function(event) {
+      moves++;
+      $("#moves").text(moves);
+
+        if (srcCount<3) {
+          $(this).children().show();
+          $source=$(this).children().attr("src");
+          srcArray.push($source);
+          srcCount++;
+        }
+      if (srcArray.length===2) {
+        if (srcArray[0]!==srcArray[1]) {
+          // $(`img[src="${srcArray[1]}"]:first`).show();
+          for (var i = 0; i < 2; i++) {
+              $(`img[src="${srcArray[i]}"]`).hide("slow");
+          }
+        }
+        else{
+          winpoint++;
+        }
+        if (winpoint===imageArray.length) {
+          alert("congrats you win")
+        }
+        srcArray=[];
+        srcCount=0;
+      }
+
+    });
 });
